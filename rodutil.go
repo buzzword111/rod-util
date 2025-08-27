42d440361e2c3e936494d1e857c486baae8b50e3
buzzword111 <buzzword111@gmail.com>
2025-08-27 23:11:21 +0900

    WaitAndHandleDialogの引数をpage *rod.Pageを受け取るように変更

--- rodutil.go
+++ rodutil.go
@@ -4,15 +4,16 @@ import (
 	"context"
 	"time"
 
+	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/proto"
 )
 
-// WaitAndHandleDialog は、Rodのダイアログイベントを指定したタイムアウト時間内で待機し、
-// ダイアログが表示された場合は handle 関数で処理し、そのダイアログ情報を返します。
+// WaitAndHandleDialog は、Rodのページで発生するJavaScriptダイアログ（alert, confirm, prompt）を、指定したタイムアウト時間内で待機し、
+// ダイアログが表示された場合は自動的に「OK」を押して閉じ、そのダイアログ情報（*proto.PageJavascriptDialogOpening）を返します。
 // タイムアウトした場合は nil を返します。
+// この関数は、ダイアログの内容を取得したい場合や、自動でダイアログを閉じたい場合に利用します。
 func WaitAndHandleDialog(
-	wait func() *proto.PageJavascriptDialogOpening,
-	handle func(bool, string),
+	page *rod.Page,
 	timeout time.Duration,
 ) *proto.PageJavascriptDialogOpening {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
@@ -21,6 +22,7 @@ func WaitAndHandleDialog(
 	done := make(chan struct{})
 	var dialog *proto.PageJavascriptDialogOpening
 
+	wait, handle := page.MustHandleDialog()
 	go func() {
 		dialog = wait()
 		close(done)
